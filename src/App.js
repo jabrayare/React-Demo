73d3c3b24068c4f487107a81196cb82b31b29e05
Aditya Chinthagunta <adityarc2@gmail.com>
2021-11-11 15:42:36 -0500

    test commit 1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Testing for changes</p>
+        <p>Testing for changes, test commit</p>
         <a
           className="App-link"
           href="https://reactjs.org"
