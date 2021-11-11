75c0cefc732dce7ac067aa29ef2911fea9a06312
jabrayare <jabra5980@gmail.com>
2021-11-11 10:56:34 -0500

    inital commit

---
+++ src/Example.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Example extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <p>THis is an class Example component</p>
+        </div>
+      </div>
+    );
+  }
+}
