e5ce984c7f51339e52bf696cf66eef529f8650e9
jabrayare <jabra5980@gmail.com>
2021-11-11 21:02:29 -0500

    added more stuff

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,21 @@
 import React from "react";
+import Team from "./Team";
+import "./about.css";
+import styled from "styled-components";
 
 function About() {
   return (
-    <div>
-      <h1>This is a bout page!</h1>
+    <div className="about">
+      <Content>
+        <h1>Meet Our Team</h1>
+
+        <Team />
+      </Content>
     </div>
   );
 }
-
+const Content = styled.div`
+  width: 80%;
+  margin: auto;
+`;
 export default About;
---
+++ src/components/Team.js
@@ -0,0 +1,107 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import styled from "styled-components";
+
+function Team() {
+  return (
+    <TeamCard>
+      <TeamContent>
+        <User className="user">
+          <FontAwesomeIcon icon={faUser} className="user" />
+        </User>
+        <p className="name">John Doe</p>
+        <p className="title">CSE Major</p>
+        <p className="description">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
+          inventore voluptatum.
+        </p>
+        <SocialLinks>
+          <FontAwesomeIcon icon={faLinkedin} className="icon linkedin" />
+          <FontAwesomeIcon icon={faGithub} className="icon github" />
+          <FontAwesomeIcon icon={faFacebook} className="icon facebook" />
+          <FontAwesomeIcon icon={faInstagram} className="icon instagram" />
+        </SocialLinks>
+      </TeamContent>
+    </TeamCard>
+  );
+}
+
+const TeamCard = styled.div`
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  width: 150px;
+  background-color: #fff;
+  box-shadow: -2px -2px 4px -4px rgba(0, 0, 0, 0.75);
+`;
+const TeamContent = styled.div`
+  width: 100%;
+  padding: 10px 4px;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
+    sans-serif Helvetica, sans-serif;
+  .name {
+    text-align: center;
+    margin: 5px;
+    font-size: 0.9rem;
+    color: #111;
+  }
+  .title {
+    text-align: center;
+    margin: 5px;
+    font-size: 0.7rem;
+    color: #676363;
+  }
+  .description {
+    text-align: left;
+    margin: 5px;
+    font-size: 0.6rem;
+    color: #111;
+    font-family: "Trebuchet MS";
+  }
+`;
+const User = styled.div`
+  width: 50px;
+  height: 50px;
+  border-radius: 100%;
+  border: 1px solid #cfcfcf;
+  background-color: #827c7c;
+  display: grid;
+  place-items: center;
+  margin: auto;
+
+  .user {
+    width: 75%;
+    height: 75%;
+    color: #131010;
+  }
+`;
+
+const SocialLinks = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 12px 10px;
+  .icon {
+    margin-right: 10px;
+    width: 15px;
+    height: 15px;
+  }
+  .facebook {
+    color: #2626f7;
+  }
+  .instagram {
+    color: #f93a00;
+  }
+  .linkedin {
+    color: #2626f7;
+  }
+  .github {
+    color: #18181f;
+  }
+`;
+export default Team;
