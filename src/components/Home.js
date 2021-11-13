01c112ae25a0caf0ac2c0779d87da108289ca0ff
Aditya Chinthagunta <adityarc2@gmail.com>
2021-11-12 22:14:03 -0500

    added some html to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,44 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1>This is Home component</h1>
+      <h1>General Statement</h1>
+      <p>Ohio State’s Department of Computer Science 
+        and Engineering has excelled in both research 
+        and education since 1968. Ranked among the top 
+        computer science departments at public institutions 
+        in the United States by the National Research 
+        Council and by U.S. News & World Report, the 
+        department provides an outstanding educational 
+        experience to 900 undergraduate and 300 graduate 
+        students from across the globe.
+        The mission of the Computer Science and Engineering 
+        Graduate Program is to develop researchers, educators 
+        and practicing professionals with advanced skills in 
+        computing. Students can pursue a master’s degree, a 
+        doctoral degree or both.
+        </p> 
+
+        <h1> space</h1>
+
+        <h1>Mission Statement</h1>
+        <p>The Department of Computer Science and Engineering 
+          will impact the information age as a national leader 
+          in computing research and education. We will prepare 
+          computing graduates who are highly sought after, 
+          productive, and well-respected for their work, and 
+          who contribute to new developments in computing. We 
+          will give students in other disciplines an appropriate 
+          foundation in computing for their education, research, 
+          and experiences after graduation, consistent with 
+          computing's increasingly fundamental role in society. 
+          In our areas of research focus, we will contribute key 
+          ideas to the development of the computing basis of the 
+          information age, advancing the state of the art for the 
+          benefit of society, the State of Ohio, and The Ohio State 
+          University. We will work with key academic partners within 
+          and outside of OSU, and with key industrial partners, in 
+          pursuit of our research and educational endeavors.
+</p>
     </div>
   );
 }
